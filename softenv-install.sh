f8f517ba6126c0faeaca1b06ece8030dfd5a0449
jhover <jhover@transit.cshl.edu>
2021-09-28 13:40:21 -0400

    Update.

--- softenv-install.sh
+++ softenv-install.sh
@@ -3,3 +3,6 @@ conda create -n xskew
 conda activate xskew
 conda install -c conda-forge -c bioconda snakemake
 conda install -c bioconda star gatk samtools bamtools
+
+
+# conda install -c conda-forge -c bioconda python snakemake=6.8.0 star gatk samtools bamtools
