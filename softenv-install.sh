fc4f51e4d1bc19df2172860d7437f0e38ec13505
jhover <jhover@transit.cshl.edu>
2021-10-06 17:04:44 -0400

    Update.

--- softenv-install.sh
+++ softenv-install.sh
@@ -1,8 +1,8 @@
 # 
 conda create -n xskew
 conda activate xskew
-conda install -c conda-forge -c bioconda snakemake
-conda install -c bioconda star gatk samtools bamtools
+#conda install -c conda-forge -c bioconda snakemake
+#conda install -c bioconda star gatk samtools bamtools sra-tools=2.11.0
 
 
-# conda install -c conda-forge -c bioconda python snakemake=6.8.0 star gatk samtools bamtools
+conda install -c conda-forge -c bioconda python snakemake=6.8.0 star gatk samtools bamtools sra-tools=2.11.0
---
+++ xskew/tools.py
@@ -0,0 +1,92 @@
+import datetime as dt
+import logging
+import subprocess
+import sys
+
+class NonZeroReturnException(Exception):
+    """
+    Thrown when a command has non-zero return code. 
+    """
+
+def setup_logging(level):
+    """ 
+    Setup logging 
+
+    """
+    FORMAT='%(asctime)s (UTC) [ %(levelname)s ] %(name)s %(filename)s:%(lineno)d %(funcName)s(): %(message)s'
+    logging.basicConfig()
+    logger = logging.getLogger()
+    streamHandler = logging.StreamHandler(sys.stdout)
+    formatter = logging.Formatter(FORMAT)
+    streamHandler.setFormatter(formatter)
+    logger.addHandler(streamHandler)
+    logger.setLevel(level)
+
+
+def run_command(cmd):
+    """
+    cmd should be standard list of tokens...  ['cmd','arg1','arg2'] with cmd on shell PATH.
+    
+    """
+    cmdstr = " ".join(cmd)
+    logging.info(f"command: {cmdstr} running...")
+    start = dt.datetime.now()
+    cp = subprocess.run(cmd, 
+                    text=True, 
+                    stdout=subprocess.PIPE, 
+                    stderr=subprocess.STDOUT)
+    end = dt.datetime.now()
+    elapsed =  end - start
+    logging.debug(f"ran cmd='{cmdstr}' return={cp.returncode} {elapsed.seconds} seconds.")
+    
+    if cp.stderr is not None:
+        logging.debug(f"got stderr: {cp.stderr}")
+    if cp.stdout is not None:
+        logging.debug(f"got stdout: {cp.stdout}")
+    
+    if str(cp.returncode) == '0':
+        logging.info(f'successfully ran {cmdstr}')
+        return(cp.stderr, cp.stdout,cp.returncode)
+    else:
+        logging.warn(f'non-zero return code for cmd {cmdstr}')
+        
+def fasterq_dump(infile, outdir, nthreads, tempdir ):
+    
+            cmd = ['fasterq-dump', 
+            '--split-files',
+            '--include-technical',
+            '--force', 
+            '--threads', nthreads,
+            '--outdir', outdir,
+            '-t', tempdir,
+            '--log-level', 'debug', 
+            infile]
+            cmdstring = ' '.join(cmd)
+            logging.debug(f'command string is {cmdstring}')
+            try:
+                run_command(cmd)
+            except NonZeroReturnException as nzre:
+                logging.error(f'problem with {infile}')
+                logging.error(traceback.format_exc(None))
+
+def star_nowasp(end1, end2, outprefix, outtemp, nthreads, genomedir):
+            
+            cmd = ['STAR',
+               '--readFilesIn', end1, end2, 
+               '--outFileNamePrefix', outprefix,
+               '--outTmpDir', outtemp, 
+               '--runThreadN', nthreads,
+               '--genomeDir', genomedir, 
+               '--twopassMode Basic',
+               '--twopass1readsN -1',
+               '--outSAMtype BAM Unsorted', 
+               '--quantMode GeneCounts'
+               ]
+            cmdstring = ' '.join(cmd)
+            logging.debug(f'command string is {cmdstring}')
+            try:
+                run_command(cmd)
+            except NonZeroReturnException as nzre:
+                logging.error(f'problem with {infile}')
+                logging.error(traceback.format_exc(None))
+    
